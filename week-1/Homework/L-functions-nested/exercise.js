4f0d1705fbc51a382c77ea183be82a70385df10a
Deniz Burcay <dbhaberal@gmail.com>
2022-02-26 15:52:47 +0100

    update

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,18 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nameInUppercase(name) {
+    return name.toUpperCase()
+}
+
+function shoutyGreeting(name) {
+    const shoutedName = nameInUppercase(name);
+    return `HELLO ${shoutedName}`;
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
---
+++ week-1/InClass/exercise-I.js
@@ -0,0 +1,8 @@
+
+function ageFunction (age) {
+    return 2022 - age;
+}
+
+let wasBorn = ageFunction (33);
+
+console.log(wasBorn);
